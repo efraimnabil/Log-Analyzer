858a7d88444f6e092d5713deb2ca41e7fe5af7ae
Efraim Nabil <efraimefefraim@gmail.com>
2024-07-10 20:46:45 +0300

    Add Scheduled to Schedule read logs in fixed time

--- src/main/java/com/service_health_monitor_portal/log_analyzer/LogAnalyzerService.java
+++ src/main/java/com/service_health_monitor_portal/log_analyzer/LogAnalyzerService.java
@@ -2,20 +2,19 @@ package com.service_health_monitor_portal.log_analyzer;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-
 import com.influxdb.client.domain.WritePrecision;
 import com.influxdb.client.write.Point;
-
-import java.time.Instant;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.time.Instant;
 import java.util.stream.Stream;
 
 @Service
@@ -25,10 +24,13 @@ public class LogAnalyzerService {
     private InfluxDBService influxDBService;
 
     private static final Logger logger = LoggerFactory.getLogger(LogAnalyzerService.class);
+
     @Value("${log.file.path}")
     private String LOG_FILE_PATH;
+
     private final ObjectMapper objectMapper = new ObjectMapper();
 
+    @Scheduled(fixedRate = 6000)
     public void analyzeLogs() {
         try (Stream<String> stream = Files.lines(Paths.get(LOG_FILE_PATH))) {
             stream.forEach(this::processLogLine);
@@ -44,24 +46,24 @@ public class LogAnalyzerService {
                 if (logNode.has("service_log")) {
                     JsonNode serviceLogNode = logNode.get("service_log");
                     logger.info("Extracted service log: {}", serviceLogNode.get("name"));
-                        
+
                     JsonNode timestamp = logNode.get("@timestamp");
                     Instant instant = Instant.now();
                     if (timestamp == null) {
                         logger.warn("No timestamp found in log line: {}", logLine);
-                    }
-                    else {
+                    } else {
                         instant = Instant.parse(timestamp.asText());
                         logger.info("Timestamp: {}", instant);
                     }
-                    
+
                     Point point = Point.measurement(serviceLogNode.get("name").asText())
-                        .addTag("id", serviceLogNode.get("id").asText())
-                        .addField("success", serviceLogNode.get("success").asInt())
-                        .addField("throttlingError", serviceLogNode.get("throttlingError").asInt())
-                        .addField("faultError", serviceLogNode.get("faultError").asInt())
-                        .addField("dependencyError", serviceLogNode.get("dependencyError").asInt())
-                        .addField("invalidInputError", serviceLogNode.get("invalidInputError").asInt()).time(instant, WritePrecision.MS);
+                            .addTag("id", serviceLogNode.get("id").asText())
+                            .addField("success", serviceLogNode.get("success").asInt())
+                            .addField("throttlingError", serviceLogNode.get("throttlingError").asInt())
+                            .addField("faultError", serviceLogNode.get("faultError").asInt())
+                            .addField("dependencyError", serviceLogNode.get("dependencyError").asInt())
+                            .addField("invalidInputError", serviceLogNode.get("invalidInputError").asInt())
+                            .time(instant, WritePrecision.MS);
 
                     influxDBService.singlePointWrite(point);
                 }
